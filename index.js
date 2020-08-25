ee4b8e76ec569ac4e1d1cc967258849db24ddf21
Adam Gold <adamgold7@gmail.com>
2020-08-25 15:46:05 +0300

    fix: 🐛 fix prototype pollution
    filter prototype, __proto__ and constructor

--- index.js
+++ index.js
@@ -75,6 +75,9 @@ api.set = function set (obj, pointer, value) {
 
     for (var i = 0; i < refTokens.length - 1; ++i) {
         var tok = refTokens[i];
+        if (tok === "__proto__" || tok === "constructor" || tok === "prototype") {
+            continue
+        }
         if (tok === '-' && Array.isArray(obj)) {
           tok = obj.length;
         }
